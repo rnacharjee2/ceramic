ae2ac6a93e086a3931ad29ccecec775bdc9b9e38
Raj N Acharjee <raj@cracktech.com.bd>
2023-05-23 13:32:58 +0600

    second commit

--- production/admin.py
+++ production/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(MeasurementUnit)
+admin.site.register(Section)
+admin.site.register(Shape)
+admin.site.register(Code)
+admin.site.register(ItemName)
+admin.site.register(RawMaterial)
+admin.site.register(Machine)
+admin.site.register(RawMaterialIncoming)
+admin.site.register(RawMaterialOutgoing)
+admin.site.register(MachineCapacity)
+admin.site.register(JiggerProduction)
--- production/models.py
+++ production/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 # Create your models here.
+SHIFT_CHOICES = [
+    (1, 'A Shift'),
+    (2, 'B Shift'),
+    (3, 'C Shift'),
+]
+
+
+class MeasurementUnit(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Section(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -13,7 +25,7 @@ class Section(models.Model):
 
 class Shape(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +33,7 @@ class Shape(models.Model):
 
 class Code(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -31,15 +43,15 @@ class ItemName(models.Model):
     name = models.CharField(max_length=100)
     shape = models.ForeignKey(Shape, on_delete=models.CASCADE)
     code = models.ForeignKey(Code, on_delete=models.CASCADE)
-    shapeCode = models.CharField(max_length=100)
-    description = models.TextField()
+    shapeCode = models.CharField(max_length=10, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     weight = models.FloatField()
     diameter = models.FloatField()
     height = models.FloatField()
-    photo = models.ImageField(upload_to='images/', blank=True)
+    photo = models.ImageField(upload_to='images/items/', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        self.shapeCode = self.shape.name + self.code.name
+        self.shapeCode = self.code.name + self.shape.name
         super(ItemName, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -48,7 +60,7 @@ class ItemName(models.Model):
 
 class RawMaterial(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -58,6 +70,9 @@ class RawMaterial(models.Model):
 class RawMaterialIncoming(models.Model):
     name = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
     quantity = models.FloatField()
+    unit = models.ForeignKey(MeasurementUnit, on_delete=models.CASCADE)
+    challanNo = models.CharField(max_length=100)
+    challanDate = models.DateField()
 
     def __str__(self):
         return self.name
@@ -66,6 +81,40 @@ class RawMaterialIncoming(models.Model):
 class RawMaterialOutgoing(models.Model):
     name = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
     quantity = models.FloatField()
+    unit = models.ForeignKey(MeasurementUnit, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class Machine(models.Model):
+    name = models.CharField(max_length=100)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MachineCapacity(models.Model):
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    item = models.ForeignKey(ItemName, on_delete=models.CASCADE)
+    capacity = models.FloatField()
+    unit = models.ForeignKey(MeasurementUnit, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.machine.name + self.item.name
+
+
+class JiggerProduction(models.Model):
+    item = models.ForeignKey(ItemName, on_delete=models.CASCADE)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    shift = models.PositiveIntegerField(choices=SHIFT_CHOICES)
+    quantity = models.PositiveIntegerField()
+    forming_loss = models.PositiveIntegerField()
+    production_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.item.name
